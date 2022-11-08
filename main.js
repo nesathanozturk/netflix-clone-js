2a88d3d28cd52d821f36a3bbc581f3dd71dd6f8f
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2022-11-08 19:16:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,37 @@
+// Elements:
 let nav = document.getElementById('nav');
-let player = document.querySelector("#video")
-let btnPlayPause = document.querySelector("#btnPlayPause")
-// Elemanları seçtik.
+let player = document.querySelector("#video");
+let btnPlayPause = document.querySelector("#btnPlayPause");
 
-// Butonun çalışması için yazılacaklar:
-
+// Functions:
 player.addEventListener("play", function() {
     changeButtonType(btnPlayPause, "Durdur");
-}, false); // "Oynat" butonuna bastığımızda buton değişip, "Durdur" butonuna dönüşecek. Bu sayede videoyu durdurabileceğiz.
+}, false);
 
 player.addEventListener("pause", function() {
-    changeButtonType(btnPlayPause, "Oynat")
-}, false); // "Durdur" butonuna bastığımızda buton değişip, "Oynat" butonuna dönüşecek. Bu sayede videoyu oynatabileceğiz.
+    changeButtonType(btnPlayPause, "Oynat");
+}, false);
 
 function playPauseVideo() {
     if (player.paused || player.ended) {
         changeButtonType(btnPlayPause, "Durdur");
-        player.play(); // Eğer video durur ya da sona ererse, "Durdur" butonuna bastığımızda video çalışır.
+        player.play();
     } else {
         changeButtonType(btnPlayPause, "Oynat");
-        player.pause(); // Eğer video çalışıyorsa, "Oynat" butonuna bastığımızda video durur.
-    }
+        player.pause();
+    };
 };
-
-// Buton değerlerinin ekranda yazılması.
 
 function changeButtonType(btn, value) {
     btn.title = value;
     btn.innerHTML = value;
 };
 
-// Sayfa hareket ettiğinde üstte oluşacak siyah navbar:
-
+// Black Navbar
 window.addEventListener("scroll", () => {
     if(window.scrollY >= 100) {
-        nav.classList.add("nav__black"); // Eğer koşula uyarsa, class'ı oluşturup, siyah navbar oluşacak.
+        nav.classList.add("nav__black");
     } else {
-        nav.classList.remove("nav__black"); // Eğer koşula uymazsa, oluşan class'ı kaldıracak ve siyah navbar gözükmeyecek.
-    }
+        nav.classList.remove("nav__black");
+    };
 });
