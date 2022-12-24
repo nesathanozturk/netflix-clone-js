0b7a89b8b224ee33973c3e937ecf8d68418f3d68
Neşathan Öztürk <107864503+nesathanozturk@users.noreply.github.com>
2022-12-24 22:40:41 +0300

    refactor: the functions are organized

--- main.js
+++ main.js
@@ -1,37 +1,45 @@
 // Elements:
-let nav = document.getElementById('nav');
+let nav = document.getElementById("nav");
 let player = document.querySelector("#video");
 let btnPlayPause = document.querySelector("#btnPlayPause");
 
 // Functions:
-player.addEventListener("play", function() {
+player.addEventListener(
+  "play",
+  function () {
     changeButtonType(btnPlayPause, "Durdur");
-}, false);
+  },
+  false
+);
 
-player.addEventListener("pause", function() {
+player.addEventListener(
+  "pause",
+  function () {
     changeButtonType(btnPlayPause, "Oynat");
-}, false);
+  },
+  false
+);
 
 function playPauseVideo() {
-    if (player.paused || player.ended) {
-        changeButtonType(btnPlayPause, "Durdur");
-        player.play();
-    } else {
-        changeButtonType(btnPlayPause, "Oynat");
-        player.pause();
-    };
-};
+  if (player.paused || player.ended) {
+    changeButtonType(btnPlayPause, "Durdur");
+    player.play();
+  } else {
+    changeButtonType(btnPlayPause, "Oynat");
+    player.pause();
+  }
+}
 
 function changeButtonType(btn, value) {
-    btn.title = value;
-    btn.innerHTML = value;
-};
+  btn.title = value;
+  btn.innerHTML = value;
+}
 
 // Black Navbar
 window.addEventListener("scroll", () => {
-    if(window.scrollY >= 100) {
-        nav.classList.add("nav__black");
-    } else {
-        nav.classList.remove("nav__black");
-    };
+  if (window.scrollY >= 100) {
+    nav.classList.add("nav__black");
+  } else {
+    nav.classList.remove("nav__black");
+  }
 });
